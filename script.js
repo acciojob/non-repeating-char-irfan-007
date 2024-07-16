3b57c12d64e9f979877fbab719e22259796b5f47
irfan <85334710+irfan-007@users.noreply.github.com>
2024-07-16 22:50:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	str.toLowerCase();
+	const alp={};
+	for(let i of str){
+		if(i>='a' && i<='z'){
+			if(i in alp)
+				alp[i]=alp[i]+1;
+			else
+				alp[i]=1;
+		}
+	}
+	for(let i in alp){
+		if(alp[i]==1)
+			return i;
+	}
+	return null;
 }
 const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+alert(firstNonRepeatedChar(input));
